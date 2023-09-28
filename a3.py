5afe04cb2928e951d558e464d6beac5959b6bff5
mjlarson2 <mjlarson2@cps.edu>
2023-09-28 10:03:47 -0500

    Implemented search_pa_list

--- a3.py
+++ a3.py
@@ -246,8 +246,19 @@ def search_pa_list(src: List[str]) -> List[str]:
     Returns:
         a list of answers. Will be ["I don't understand"] if it finds no matches and
         ["No answers"] if it finds a match but no answers
+
+
     """
-    pass
+    output = []
+    for tuple in pa_list:
+        keywords = match(tuple[0], src)
+        if keywords != None:
+            output = tuple[1](keywords)
+            if output != []: return output
+            else: return ["No answers"]
+    return ["I don't understand"]
+
+    
 
 
 def query_loop() -> None:
